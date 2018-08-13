f4f1e293c53034f7a6a4f02e63beadff3d78c3f2
Fabien JUIF <fabien.juif@gmail.com>
2018-08-13 16:10:44 +0200

    :sparkles: can choose renderer (debug purpose)

--- src/index.js
+++ src/index.js
@@ -8,33 +8,11 @@ import Player from './sprites/player'
 import Skill from './skills/skill'
 import IA from './inputs/ia'
 import LocalInputs from './inputs/local'
+import Renderer from './renderer/renderer'
 
 // create an engine
 var engine = Engine.create()
 engine.world.gravity = { x: 0, y: 0 }
-
-// FIXME: move it to a directory one we need debug
-// create a renderer
-// var render = Render.create({
-//   element: document.body,
-//   engine: engine,
-//   options: {
-//     width: window.innerWidth,
-//     height: window.innerHeight,
-//     pixelRatio: 0.8,
-//     wireframeBackground: '#222',
-//     hasBounds: false,
-//     wireframes: true,
-//     showDebug: true,
-//     showBroadphase: true,
-//     showBounds: true,
-//     showVelocity: true,
-//     showAxes: true,
-//     showPositions: true,
-//     showIds: true,
-//     showVertexNumbers: true,
-//   },
-// })
 
 const WORLD_SIZE = {
   x: 1600,
@@ -51,32 +29,15 @@ const walls = [
   Wall.create(0, WORLD_SIZE.y - 100, WORLD_SIZE.x, 100), //
 ]
 
-// add all of the bodies to the world
 World.add(engine.world, [player.physics, enemy.physics, ...walls.map(({ physics }) => physics)])
 
-// run the engine
 Engine.run(engine)
 
-// run the renderer
-// Render.run(render)
-
-const renderer = PIXI.autoDetectRenderer(innerWidth, innerHeight, {
-  backgroundColor: 0x000000,
-})
-const stage = new PIXI.Container()
-document.body.appendChild(renderer.view)
-const viewport = new Viewport({
-  screenWidth: window.innerWidth,
-  screenHeight: window.innerHeight,
-  worldWidth: WORLD_SIZE.x,
-  worldHeight: WORLD_SIZE.y,
-})
-viewport.follow(player.physics.position, { speed: 20, radius: 100 })
-viewport.addChild(player.graphics)
-viewport.addChild(enemy.graphics)
-walls.forEach(wall => viewport.addChild(wall.graphics))
-
-stage.addChild(viewport)
+const renderer = Renderer.create(window.innerWidth, window.innerHeight, WORLD_SIZE.x, WORLD_SIZE.y, /* { matter: engine } */)
+Renderer.follow(renderer, player)
+Renderer.addToViewport(renderer, player)
+Renderer.addToViewport(renderer, enemy)
+walls.forEach(wall => Renderer.addToViewport(renderer, wall))
 
 // draw walls only once since it doesnt move
 Wall.draw(walls)
@@ -96,25 +57,24 @@ const ui = () => {
   print.push(`${Math.floor(player.hp)} HP`)
   print.push(`${Math.floor(enemy.hp)} E.HP`)
 
-  if (lastUI) stage.removeChild(lastUI)
-  lastUI = stage.addChild(new PIXI.Text(print.join(' | '), { fill: 'white', fontFamily: 'Courier New', fontSize: 20 }))
+  if (lastUI) Renderer.removeFromStage(renderer, { graphics: lastUI })
+  lastUI = Renderer.addToStage(renderer, { graphics: new PIXI.Text(print.join(' | '), { fill: 'white', fontFamily: 'Courier New', fontSize: 20 }) })
 
-  if (lastFPS) stage.removeChild(lastFPS)
+  if (lastFPS) Renderer.removeFromStage(renderer, { graphics: lastFPS })
   var thisLoop = Date.now()
   const fps = new PIXI.Text(1000 / (thisLoop - lastLoop), { fill: 'white', fontFamily: 'Courier New', fontSize: 20 })
   fps.position.y = window.innerHeight - 30
   fps.position.x = window.innerWidth - 50
-  lastFPS = stage.addChild(fps)
+  lastFPS = Renderer.addToStage(renderer, { graphics: fps })
   lastLoop = thisLoop
 }
 
+// TODO: move it to renderer object
 const renderPixi = () => {
   Player.draw(player)
   Player.draw(enemy)
 
   ui()
-
-  renderer.render(stage)
 }
 
 let lastUI
@@ -167,8 +127,7 @@ const loop = () => {
 
   // draw
   renderPixi()
-
-  // Render.lookAt(render, boxA, { x: 500, y: 500 })
+  Renderer.update(renderer)
 
   engine.world.bodies.forEach(body => Body.setAngle(body, 0))
 }
---
+++ src/renderer/renderer.js
@@ -0,0 +1,111 @@
+import * as PIXI from 'pixi.js'
+import Viewport from 'pixi-viewport'
+import { Render } from 'matter-js'
+
+const create = (screenWidth, screenHeight, worldWidth, worldHeight, { matter = false } = {}) => {
+  let renderer
+  let viewport
+  let stage
+
+  if (matter) {
+    renderer = Render.create({
+      element: document.body,
+      engine: matter,
+      options: {
+        width: screenWidth,
+        height: screenHeight,
+        pixelRatio: 0.8,
+        wireframeBackground: '#222',
+        hasBounds: false,
+        wireframes: true,
+        showDebug: true,
+        showBroadphase: true,
+        showBounds: true,
+        showVelocity: true,
+        showAxes: true,
+        showPositions: true,
+        showIds: true,
+        showVertexNumbers: true,
+      },
+    })
+
+    Render.run(renderer)
+  } else {
+    renderer = PIXI.autoDetectRenderer(screenWidth, screenHeight, {
+      backgroundColor: 0x000000,
+    })
+    stage = new PIXI.Container()
+    document.body.appendChild(renderer.view)
+    viewport = new Viewport({
+      screenWidth,
+      screenHeight,
+      worldWidth,
+      worldHeight,
+    })
+    stage.addChild(viewport)
+  }
+
+  return {
+    follow: undefined,
+    renderer,
+    viewport,
+    stage,
+    matter,
+    width: screenWidth,
+    height: screenHeight,
+  }
+}
+
+const update = renderer => {
+  const { matter, follow } = renderer
+
+  console.log({ matter, follow })
+
+  if (matter) {
+    if (follow) {
+      Render.lookAt(renderer.renderer, follow.physics, { x: 500, y: 500 })
+    }
+  } else {
+    renderer.renderer.render(renderer.stage)
+  }
+}
+
+const addToViewport = (renderer, object) => {
+  const { matter, viewport } = renderer
+  if (matter) return
+
+  return viewport.addChild(object.graphics)
+}
+
+const addToStage = (renderer, object) => {
+  const { matter, stage } = renderer
+  if (matter) return
+
+  return stage.addChild(object.graphics)
+}
+
+const removeFromStage = (renderer, object) => {
+  const { matter, stage } = renderer
+  if (matter) return
+
+  return stage.removeChild(object.graphics)
+}
+
+const follow = (renderer, object) => {
+  const { matter, viewport } = renderer
+
+  renderer.follow = object
+
+  if (!matter) {
+    viewport.follow(object.physics.position, { speed: 20, radius: 100 })
+  }
+}
+
+export default {
+  create,
+  update,
+  addToStage,
+  removeFromStage,
+  addToViewport,
+  follow
+}

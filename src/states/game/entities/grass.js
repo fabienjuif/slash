a16ff1a34c024697e3db945afd258e7e86ac2ff0
Fabien JUIF <fabien.juif@gmail.com>
2018-08-24 23:10:12 +0200

    :zap: remove grass as sprite

--- src/states/game/entities/grass.js
+++ src/states/game/entities/grass.js
@@ -1,44 +1,15 @@
-import { Container } from 'pixi.js'
+import { Graphics } from 'pixi.js'
 import { Common } from 'matter-js'
 import Sprites from '../../../sprites'
 
 const create = ({ width, height }) => {
   const entity = {
-    graphics: new Container(),
+    graphics: new Graphics(),
     drew: false,
   }
 
-  const sprites = Sprites.create()
-  Promise
-    .all(Sprites.load(sprites, '/static-textures.json', false))
-    .then(() => {
-      Array
-        .from({ length: Math.round(width / 32) })
-        .forEach((valueX, indexX) => {
-          Array
-            .from({ length: Math.round(height / 32) })
-            .forEach((valueY, indexY) => {
-              const sprite = entity.graphics.addChild(Sprites.asTilingSprites(
-                sprites,
-                Common.choose([
-                  'generic-rpg-tile01',
-                  'generic-rpg-tile03',
-                  'generic-rpg-tile07',
-                  'generic-rpg-tile11',
-                  'generic-rpg-tile17',
-                  'generic-rpg-tile18',
-                  ...Array.from({ length: 400 }).map(() => 'generic-rpg-tile38'),
-                ]),
-              ))
-              sprite.scale = { x: 2, y: 2 }
-              sprite.position.x = indexX * 32
-              sprite.position.y = indexY * 32
-            })
-        })
-
-      // cache as bitmap (perf)
-      entity.graphics.cacheAsBitmap = true
-    })
+  entity.graphics.beginFill(0x00b897)
+  entity.graphics.drawRect(0, 0, width, height)
 
   return entity
 }
